d9d4664f47a83813df595c1e611abe0902ca2988
faithadewuyi <iyanuleke09@gmail.com>
2024-05-28 14:18:25 +0100

    data attribute

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const tweetBtn = document.getElementById('tweet-btn')
 tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
-
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+        console.log(e.target.dataset.like)
+    }
+   
+})
 function getFeed(){
     let feedHTML = ``
     tweetsData.forEach(function(tweet){
@@ -20,16 +25,15 @@ function getFeed(){
             <p class="tweet-text">${tweet.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
-
-                <i class="fa-solid fa-comment-dots"></i>
+                <i class="fa-solid fa-comment-dots" data-replies='${tweet.uuid}'></i>
                   ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-heart"></i>
+                <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
                    ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-retweet"></i>
+                <i class="fa-solid fa-retweet" data-retweet='${tweet.uuid}'></i>
                     ${tweet.retweets}
                 </span>
             </div>   
