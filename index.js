0736d29024eaeaaf4b4e1f71eba871d027c5f5f0
faithadewuyi <iyanuleke09@gmail.com>
2024-06-10 16:16:16 +0100

    replies displayed

--- index.js
+++ index.js
@@ -63,9 +63,23 @@ function getFeed(){
             reweetIconClass = 'retweeted'
          }
 
+         let repliesHtml = ''
+
          if(tweet.replies.length > 0)
             {
-                
+                tweet.replies.forEach(function(reply){
+                    repliesHtml+=`
+                    <div class="tweet-reply">
+                <div class="tweet-inner">
+                    <img src="${reply.profilePic}" class="profile-pic">
+                        <div>
+                            <p class="handle">${reply.handle}</p>
+                            <p class="tweet -text">${reply.tweetText}</p>
+                        </div>
+                    </div>
+            </div>
+                    `
+                })
             }
 
         feedHTML += `
@@ -91,6 +105,11 @@ function getFeed(){
             </div>   
         </div>            
     </div>
+    <div id ="replies-${tweet.uuid}">
+    ${repliesHtml}
+    </div>
+
+    
 </div>
         `
     })
